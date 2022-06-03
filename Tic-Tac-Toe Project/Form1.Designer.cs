51a9237d581119d1f2796c134ae4590ceac27c43
unknown <migzberneri@gmail.com>
2022-06-03 11:05:52 +0800

    First commit, 9 buttons added and renamed

---
+++ Tic-Tac-Toe Project/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Tic_Tac_Toe_Project
+{
+    partial class _form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._bn1 = new System.Windows.Forms.Button();
+            this._bn2 = new System.Windows.Forms.Button();
+            this._bn3 = new System.Windows.Forms.Button();
+            this._bn4 = new System.Windows.Forms.Button();
+            this._bn5 = new System.Windows.Forms.Button();
+            this._bn6 = new System.Windows.Forms.Button();
+            this._bn7 = new System.Windows.Forms.Button();
+            this._bn8 = new System.Windows.Forms.Button();
+            this._bn9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _bn1
+            // 
+            this._bn1.Location = new System.Drawing.Point(235, 60);
+            this._bn1.Name = "_bn1";
+            this._bn1.Size = new System.Drawing.Size(146, 101);
+            this._bn1.TabIndex = 0;
+            this._bn1.Text = "button1";
+            this._bn1.UseVisualStyleBackColor = true;
+            // 
+            // _bn2
+            // 
+            this._bn2.Location = new System.Drawing.Point(235, 167);
+            this._bn2.Name = "_bn2";
+            this._bn2.Size = new System.Drawing.Size(146, 101);
+            this._bn2.TabIndex = 1;
+            this._bn2.Text = "button2";
+            this._bn2.UseVisualStyleBackColor = true;
+            // 
+            // _bn3
+            // 
+            this._bn3.Location = new System.Drawing.Point(235, 274);
+            this._bn3.Name = "_bn3";
+            this._bn3.Size = new System.Drawing.Size(146, 103);
+            this._bn3.TabIndex = 2;
+            this._bn3.Text = "button3";
+            this._bn3.UseVisualStyleBackColor = true;
+            // 
+            // _bn4
+            // 
+            this._bn4.Location = new System.Drawing.Point(423, 60);
+            this._bn4.Name = "_bn4";
+            this._bn4.Size = new System.Drawing.Size(146, 101);
+            this._bn4.TabIndex = 3;
+            this._bn4.Text = "button4";
+            this._bn4.UseVisualStyleBackColor = true;
+            // 
+            // _bn5
+            // 
+            this._bn5.Location = new System.Drawing.Point(423, 167);
+            this._bn5.Name = "_bn5";
+            this._bn5.Size = new System.Drawing.Size(146, 101);
+            this._bn5.TabIndex = 4;
+            this._bn5.Text = "button5";
+            this._bn5.UseVisualStyleBackColor = true;
+            // 
+            // _bn6
+            // 
+            this._bn6.Location = new System.Drawing.Point(423, 274);
+            this._bn6.Name = "_bn6";
+            this._bn6.Size = new System.Drawing.Size(146, 103);
+            this._bn6.TabIndex = 5;
+            this._bn6.Text = "button6";
+            this._bn6.UseVisualStyleBackColor = true;
+            // 
+            // _bn7
+            // 
+            this._bn7.Location = new System.Drawing.Point(599, 60);
+            this._bn7.Name = "_bn7";
+            this._bn7.Size = new System.Drawing.Size(146, 101);
+            this._bn7.TabIndex = 6;
+            this._bn7.Text = "button7";
+            this._bn7.UseVisualStyleBackColor = true;
+            // 
+            // _bn8
+            // 
+            this._bn8.Location = new System.Drawing.Point(599, 167);
+            this._bn8.Name = "_bn8";
+            this._bn8.Size = new System.Drawing.Size(146, 101);
+            this._bn8.TabIndex = 7;
+            this._bn8.Text = "button8";
+            this._bn8.UseVisualStyleBackColor = true;
+            // 
+            // _bn9
+            // 
+            this._bn9.Location = new System.Drawing.Point(599, 274);
+            this._bn9.Name = "_bn9";
+            this._bn9.Size = new System.Drawing.Size(146, 103);
+            this._bn9.TabIndex = 8;
+            this._bn9.Text = "button9";
+            this._bn9.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(890, 33);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.exitToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(73, 29);
+            this.toolStripMenuItem1.Text = "Menu";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 31);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem.Text = "Exit game";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // _form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 525);
+            this.Controls.Add(this._bn9);
+            this.Controls.Add(this._bn8);
+            this.Controls.Add(this._bn7);
+            this.Controls.Add(this._bn6);
+            this.Controls.Add(this._bn5);
+            this.Controls.Add(this._bn4);
+            this.Controls.Add(this._bn3);
+            this.Controls.Add(this._bn2);
+            this.Controls.Add(this._bn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "_form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button _bn1;
+        private Button _bn2;
+        private Button _bn3;
+        private Button _bn4;
+        private Button _bn5;
+        private Button _bn6;
+        private Button _bn7;
+        private Button _bn8;
+        private Button _bn9;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripTextBox toolStripTextBox1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ Tic-Tac-Toe Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tic_Tac_Toe_Project
+{
+    public partial class _form1 : Form
+    {
+        public _form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
